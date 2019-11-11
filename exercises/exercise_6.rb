22930d6ec9cc28ef5b47b22caa0edf9dbf8185d1
Fabio Papa <fabtheman@gmail.com>
2019-11-11 15:34:58 -0800

    Complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -5,7 +7,29 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
-puts "Exercise 6"
-puts "----------"
+puts 'Exercise 6'
+puts '----------'
 
-# Your code goes here ...
+@store1.employees.create(
+  first_name: 'Khurram',
+  last_name: 'Virani',
+  hourly_rate: 60
+)
+
+@store1.employees.create(
+  first_name: 'Joe',
+  last_name: 'Smith',
+  hourly_rate: 45
+)
+
+@store2.employees.create(
+  first_name: 'Thomas',
+  last_name: 'Square',
+  hourly_rate: 85
+)
+
+@store2.employees.create(
+  first_name: 'Fabio',
+  last_name: 'Papa',
+  hourly_rate: 300
+)
