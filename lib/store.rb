7bda1565d20ad6374b816b0d2fd0aceb8a3c63c4
Fabio Papa <fabtheman@gmail.com>
2019-11-11 17:21:07 -0800

    Complete exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -11,10 +11,18 @@ class Store < ActiveRecord::Base
 
   has_many :employees
 
+  before_destroy :check_for_employees
+
   def mens_apparel_or_womens_apparel_must_be_true
     if !mens_apparel && !womens_apparel
       errors.add(:mens_apparel, "can't be false if womens apparel is also false")
       errors.add(:womens_apparel, "can't be false if mens apparel is also false")
     end
+  end
+
+  private
+
+  def check_for_employees
+    return false if employees.count.positive?
   end
 end
